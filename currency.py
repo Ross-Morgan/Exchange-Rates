f18d2831c97d9cc21f1158bbd1c7f3bb8264f77e
Ross Morgan <morgan.ross.512@gmail.com>
2021-12-10 01:12:19 +0000

    cache for codes

--- currency.py
+++ currency.py
@@ -1,38 +1,42 @@
 from datapackage import Package as Pkg
+from typing import Optional
+import os
 
 
-def get_codes():
+def get_cache() -> Optional[list[str]]:
+    if os.path.exists("codes.csv"):
+        return open("codes.csv").read().split(",")
+
+
+def cache_codes(codes: list[str]):
+    if os.path.exists("codes.csv"):
+        return
+
+    open("codes.csv", "w").write(",".join(codes))
+
+
+def get_codes() -> list[str]:
+    cache = get_cache()
+
+    print(f"{cache=}")
+
+    if cache:
+        return cache
+
     package = Pkg('https://datahub.io/core/currency-codes/datapackage.json')
     codes: list[str] = []
 
     for resource in package.resources:
-        if resource.descriptor['datahub']['type'] == 'derived/csv':
-            codes = list(map(lambda x: x[2], resource.read()))
+        if resource.descriptor["datahub"]["type"] != "derived/csv":
+            continue
 
-            while None in codes:
-                codes.remove(None)
+        codes = list(map(lambda x: x[2], resource.read()))
+
+        while None in codes:
+            codes.remove(None)
+
+        break
+
+    cache_codes(codes)
 
     return codes
-
-
-# currency_codes = (
-#     "JPY", "CNY", "SDG", "RON", "MKD", "MXN", "CAD", "XAF", "GYD", "AFN",
-#     "ZAR", "AUD", "NOK", "ILS", "ISK", "SYP", "LYD", "UYU", "YER", "CSD",
-#     "EEK", "THB", "IDR", "LBP", "AED", "BOB", "QAR", "BHD", "HNL", "HRK",
-#     "COP", "ALL", "DKK", "MYR", "SEK", "RSD", "BGN", "DOP", "KRW", "LVL",
-#     "VEF", "CZK", "TND", "KWD", "VND", "JOD", "NZD", "PAB", "CLP", "PEN",
-#     "GBP", "DZD", "CHF", "RUB", "UAH", "ARS", "SAR", "EGP", "INR", "PYG",
-#     "TWD", "TRY", "BAM", "OMR", "SGD", "MAD", "BYR", "NIO", "HKD", "LTL",
-#     "SKK", "GTQ", "BRL", "EUR", "HUF", "IQD", "CRC", "PHP", "SVC", "PLN",
-#     "USD", "XBB", "XBC", "XBD", "UGX", "MOP", "SHP", "TTD", "UYI", "KGS",
-#     "DJF", "BTN", "XBA", "HTG", "BBD", "XAU", "FKP", "MWK", "PGK", "XCD",
-#     "COU", "RWF", "NGN", "BSD", "XTS", "TMT", "SOS", "TOP", "AOA", "KPW",
-#     "GEL", "VUV", "FJD", "MVR", "AZN", "MNT", "MGA", "WST", "KMF", "GNF",
-#     "SBD", "BDT", "MMK", "TJS", "CVE", "MDL", "KES", "SRD", "LRD", "MUR",
-#     "CDF", "BMD", "USN", "CUP", "USS", "GMD", "UZS", "CUC", "ZMK", "NPR",
-#     "NAD", "LAK", "SZL", "XDR", "BND", "TZS", "MXV", "LSL", "KYD", "LKR",
-#     "ANG", "PKR", "SLL", "SCR", "GHS", "ERN", "BOV", "GIP", "IRR", "XPT",
-#     "BWP", "XFU", "CLF", "ETB", "STD", "XXX", "XPD", "AMD", "XPF", "JMD",
-#     "MRO", "BIF", "CHW", "ZWL", "AWG", "MZN", "CHE", "XOF", "KZT", "BZD",
-#     "XAG", "KHR",
-# )
