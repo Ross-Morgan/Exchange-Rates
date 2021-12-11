79fbc6d33e24dc402f97a4d2c64d50bf1b08b51e
Ross Morgan <morgan.ross.512@gmail.com>
2021-12-11 14:06:37 +0000

    commit

--- currency.py
+++ currency.py
@@ -1,7 +1,8 @@
 # Stdlib imports
-from datetime import date
+from datetime import datetime
 from typing import Optional
 import os
+import re
 
 # Install module imports
 from forex_python.converter import CurrencyCodes, CurrencyRates
@@ -23,8 +24,8 @@ def get_codes_cache() -> Optional[list[str]]:
         return open(CODES_PATH).read().strip().split(",")
 
 
-def cache_currency_codes(codes: set[str]):
-    open(CODES_PATH, "w+").write(",".join(codes))
+def cache_currency_codes(currency_codes: set[str]):
+    open(CODES_PATH, "w+").write(",".join(currency_codes))
 
 
 def get_country() -> str:
@@ -38,22 +39,22 @@ def get_codes() -> set[str]:
         return set(cache)
 
     package = Pkg("https://datahub.io/core/currency-codes/datapackage.json")
-    codes: list[str] = set()
+    currency_codes: list[str] = set()
 
     for resource in package.resources:
         if resource.descriptor["datahub"]["type"] != "derived/csv":
             continue
 
-        codes = set(map(lambda x: x[2], resource.read()))
+        currency_codes = set(map(lambda x: x[2], resource.read()))
 
-        while None in codes:
-            codes.remove(None)
+        while None in currency_codes:
+            currency_codes.remove(None)
 
         break
 
-    cache_currency_codes(codes)
+    cache_currency_codes(currency_codes)
 
-    return codes
+    return currency_codes
 
 
 def set_default_currency(code: str):
@@ -69,9 +70,19 @@ def get_symbol(code: str) -> str:
     return codes.get_symbol(code)
 
 
-def get_rate(code1: str, code2: str, date: date = None) -> float:
+def remove_symbol(val: str) -> float:
+    if not val.isdecimal():
+        return 0.0
+    return float(re.compile(r"[^\d.]+").sub("", val))
+
+
+def get_rate(code1: str, code2: str, date: datetime = None) -> float:
     return rates.get_rate(code1, code2, date)
 
 
-def convert(code1: str, code2: str, val: float, date: date) -> float:
+def convert(code1: str, code2: str, val: float, date: datetime) -> float:
     return rates.convert(code1, code2, val, date)
+
+
+if __name__ == "__main__":
+    print(remove_symbol("US$69,420.69"))
