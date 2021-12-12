36f28d6109652fcd70fd67faaf1575205e4534d4
Ross Morgan <morgan.ross.512@gmail.com>
2021-12-12 01:48:15 +0000

    commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 from json.decoder import JSONDecodeError
 from typing import Optional, Union
 
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg, QtOpenGL
 
 from currency import convert as _convert
 from currency import floatify, get_codes, get_default_currency
@@ -17,28 +17,132 @@ def qss_sheet(name) -> str:
     return open(f"Assets/Scripts/{name}.qss").read()
 
 
-class GraphWindow(QtWidgets.QWidget):
-    def __init__(self, size: tuple[int, int], title: str,
-                 icon: QtGui.QIcon,
-                 parent: Optional[QtWidgets.QWidget] = None) -> None:
-        super(GraphWindow, self).__init__(parent)
+class SvgView(QtWidgets.QGraphicsView):
+    Native, OpenGL, Image = range(3)
 
-        self.setFixedSize(*size)
-        self.setWindowTitle(title)
-        self.setWindowIcon(icon)
+    def __init__(self, parent=None):
+        super(SvgView, self).__init__(parent)
 
-        self.setup_ui()
+        self.renderer = SvgView.Native
+        self.svgItem = None
+        self.backgroundItem = None
+        self.outlineItem = None
+        self.image = QtGui.QImage()
 
-    def setup_ui(self):
-        self.graph = QtWidgets.QLabel(self)
-        self.graph.setGeometry(0, 0, self.width(), self.height())
+        self.setScene(QtWidgets.QGraphicsScene(self))
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        self.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
 
-    def show(self):
-        self.graph.setPixmap(QtGui.QPixmap("graph.svg").scaled(
-            self.width(), self.height())
-        )
+        # Prepare background check-board pattern.
+        tilePixmap = QtGui.QPixmap(64, 64)
+        tilePixmap.fill(QtCore.Qt.white)
+        tilePainter = QtGui.QPainter(tilePixmap)
+        color = QtGui.QColor(220, 220, 220)
+        tilePainter.fillRect(0, 0, 32, 32, color)
+        tilePainter.fillRect(32, 32, 32, 32, color)
+        tilePainter.end()
 
-        super().show()
+        self.setBackgroundBrush(QtGui.QBrush(tilePixmap))
+
+    def drawBackground(self, p, rect):
+        p.save()
+        p.resetTransform()
+        p.drawTiledPixmap(self.viewport().rect(),
+                          self.backgroundBrush().texture())
+        p.restore()
+
+    def openFile(self, svg_file):
+        if not svg_file.exists():
+            return
+
+        s = self.scene()
+
+        if self.backgroundItem:
+            drawBackground = self.backgroundItem.isVisible()
+        else:
+            drawBackground = False
+
+        if self.outlineItem:
+            drawOutline = self.outlineItem.isVisible()
+        else:
+            drawOutline = True
+
+        s.clear()
+        self.resetTransform()
+
+        self.svgItem = QtSvg.QGraphicsSvgItem(svg_file.fileName())
+        self.svgItem.setFlags(QtWidgets.QGraphicsItem.ItemClipsToShape)
+        self.svgItem.setCacheMode(QtWidgets.QGraphicsItem.NoCache)
+        self.svgItem.setZValue(0)
+
+        self.backgroundItem = QtWidgets.QGraphicsRectItem(self.svgItem.
+                                                          boundingRect())
+        self.backgroundItem.setBrush(QtCore.Qt.white)
+        self.backgroundItem.setPen(QtGui.QPen(QtCore.Qt.NoPen))
+        self.backgroundItem.setVisible(drawBackground)
+        self.backgroundItem.setZValue(-1)
+
+        self.outlineItem = QtWidgets.QGraphicsRectItem(self.svgItem.
+                                                       boundingRect())
+        outline = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.DashLine)
+        outline.setCosmetic(True)
+        self.outlineItem.setPen(outline)
+        self.outlineItem.setBrush(QtGui.QBrush(QtCore.Qt.NoBrush))
+        self.outlineItem.setVisible(drawOutline)
+        self.outlineItem.setZValue(1)
+
+        s.addItem(self.backgroundItem)
+        s.addItem(self.svgItem)
+        s.addItem(self.outlineItem)
+
+        s.setSceneRect(self.outlineItem.boundingRect()
+                       .adjusted(-10, -10, 10, 10))
+
+    def setRenderer(self, renderer):
+        self.renderer = renderer
+
+        if self.renderer == SvgView.OpenGL:
+            if QtOpenGL.QGLFormat.hasOpenGL():
+                self.setViewport(QtOpenGL.QGLWidget(
+                                  QtOpenGL.QGLFormat(
+                                   QtOpenGL.QGL.SampleBuffers)))
+        else:
+            self.setViewport(QtWidgets.QWidget())
+
+    def setHighQualityAntialiasing(self, highQualityAntialiasing):
+        if QtOpenGL.QGLFormat.hasOpenGL():
+            self.setRenderHint(QtGui.QPainter.HighQualityAntialiasing,
+                               highQualityAntialiasing)
+
+    def setViewBackground(self, enable):
+        if self.backgroundItem:
+            self.backgroundItem.setVisible(enable)
+
+    def setViewOutline(self, enable):
+        if self.outlineItem:
+            self.outlineItem.setVisible(enable)
+
+    def paintEvent(self, event):
+        if self.renderer == SvgView.Image:
+            if self.image.size() != self.viewport().size():
+                self.image = QtGui.QImage(self.viewport().size(),
+                                          QtGui.QImage.
+                                          Format_ARGB32_Premultiplied)
+
+            imagePainter = QtGui.QPainter(self.image)
+            QtWidgets.QGraphicsView.render(self, imagePainter)
+            imagePainter.end()
+
+            p = QtGui.QPainter(self.viewport())
+            p.drawImage(0, 0, self.image)
+        else:
+            super(SvgView, self).paintEvent(event)
+
+    def wheelEvent(self, event):
+        factor = pow(1.2, event.delta() / 240.0)
+        self.scale(factor, factor)
+        event.accept()
 
 
 class MainWindowWrapper(QtWidgets.QMainWindow):
@@ -85,10 +189,6 @@ class MainWindow(MainWindowWrapper):
         self.setup_ui()
         self.setup_shortcuts()
 
-    def subwindow(self, size: tuple[int, int], title: str,
-                  icon: QtGui.QIcon) -> GraphWindow:
-        return GraphWindow(size, title, icon, self)
-
     def setup_ui(self) -> None:
         _insert_policy = QtWidgets.QComboBox.InsertPolicy.InsertAlphabetically
         _default_currs = ("GBP", "USD")
@@ -96,7 +196,8 @@ class MainWindow(MainWindowWrapper):
         with suppress(ValueError):
             _default_currs = get_default_currency()
 
-        self.graph_window = self.subwindow((800, 400), "Currency Graph",
+        self.graph_window = self.subwindow((1500, int(1500 // 2.34655532)),
+                                           "Currency Graph",
                                            QtGui.QIcon(self.icon))
 
         self.ui["background"] = QtWidgets.QLabel(self)
@@ -209,9 +310,6 @@ class MainWindow(MainWindowWrapper):
 
         self.ui["pixmap"] = QtGui.QPixmap("graph.svg")
         self.ui["graph"].setPixmap(self.ui["pixmap"].scaled(380, 190))
-
-    def show_graph(self):
-        self.graph_window.show()
 
 
 def main():
